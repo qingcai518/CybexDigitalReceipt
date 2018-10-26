18af7dda1edc9679ab31bf3a4a76803845288d44
Qingcai <qingcai518@gmail.com>
2018-10-26 12:28:59 +0900

    fix bug

--- src/api/CybexAPI.py
+++ src/api/CybexAPI.py
@@ -355,10 +355,8 @@ def transfer():
     try:
         result = wallet_transfer(from_name=from_name, to_name=to_name, amount=amount, symbol=symbol, private_key=private_key, public_key=public_key, memo=memo)
         return response(result)
-    except Exception as e:
-        print(e)
-        msg = e.args[len(e.args) - 1]
-        return error_handler(msg, 400)
+    except Exception:
+        return error_handler("fail to transfer", 400)
 
 
 
