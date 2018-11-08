ef6a7a3023cecd36e94bb3518f3d10207e696725
Qingcai <qingcai518@gmail.com>
2018-11-08 15:20:52 +0900

    status

--- src/bot/JCTbot.py
+++ src/bot/JCTbot.py
@@ -4,9 +4,6 @@ import hmac
 import hashlib
 import requests
 
-# api_key = 'b4eec5db92db4530a6758f47cd78e9b4'
-# api_secret = 'e851bba3ca8ad898930c5edcbb80d41a'
-# memo = 'JCT'
 api_key = 'be4dd2ef02bf6c7205da170e89b99e96'
 api_secret = 'e06791211692c0df9e79a90e0fe7cbf5'
 memo = 'jctbot'
@@ -57,7 +54,7 @@ def get_headers():
     return headers
 
 
-## 仓位.
+## 用户资产.
 def walletBalance():
     url = "https://openapi.bitmart.com/v2/wallet"
     headers = get_headers()
@@ -101,6 +98,7 @@ def placeOrder(amount, price, side, symbol):
 def listOrders(symbol, status, offset, limit):
     print("==== List Orders ====")
     url = "https://openapi.bitmart.com/v2/orders?symbol={0}&status={1}&offset={2}&limit={3}".format(symbol, status, offset, limit)
+    print(url)
     headers = get_headers()
     response = requests.get(url=url, headers=headers)
     print(response.text)
@@ -109,17 +107,16 @@ def listOrders(symbol, status, offset, limit):
 ## 取消订单.
 def cancelOrder(order_id):
     print("==== Cancel Order ====")
-    url = "https://openapi.bitmart.com/v2/orders/1223181"
-    headers = get_headers()
+    url = "https://openapi.bitmart.com/v2/orders/{0}".format(order_id)
+
     message = "entrust_id={0}".format(order_id)
     signature = create_sha256_signature(message=message)
+
+    headers = get_headers()
     headers["X-BM-SIGNATURE"] = signature
 
     response = requests.delete(url=url, headers=headers)
     print(response.text)
 
 
-# 讲刚才的两笔订单取消.
-# cancelOrder("57098199")
-# cancelOrder("57099437")
-
+listOrders(symbol="JCT_ETH", status=3, offset=0, limit=100)
